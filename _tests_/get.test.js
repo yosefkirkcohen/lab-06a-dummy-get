800f09f91360cafd40b4128d83a447152ded2d9a
yosefkirkcohen <ycohen45@yahoo.com>
2021-09-28 16:45:49 -0700

    tests working

--- _tests_/get.test.js
+++ _tests_/get.test.js
@@ -1,4 +1,4 @@
-const { app } = require('../index.js');
+const { app } = require('../server.js');
 const supertest = require('supertest');
 const request = supertest(app);
 
--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-const express = require('express')
-const app = express()
-const { chessPlayers } = require('./data.js')
-const cors = require('cors')
-
-app.use(cors())
-
-app.get('/chess-players', (req, res) => {
-  res.json(chessPlayers);
-})
-
-app.get('/chess-players/:id', (req,res) => {
-    const matchingPlayer = chessPlayers.find(player => player.id === Number(req.params.id))
-    res.json(matchingPlayer);
-})
+const { app } = require('./server.js')
 
 
 const port = process.env.PORT || 3001;
