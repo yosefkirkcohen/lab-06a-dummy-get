3744d21651c15fce5881a495b951d284d84a318c
yosefkirkcohen <ycohen45@yahoo.com>
2021-09-28 15:57:18 -0700

    deployment

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-
+const express = require('express')
+const app = express()
+const { chessPlayers } = require('./data.js')
+const cors = require('cors')
 
 app.use(cors())
+
+app.get('/chess-players', (req, res) => {
+  res.json(chessPlayers);
+})
+
+app.get('/chess-players/:id', (req,res) => {
+    const matchingPlayer = chessPlayers.find(player => player.id === Number(req.params.id))
+    res.json(matchingPlayer);
+})
+
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
